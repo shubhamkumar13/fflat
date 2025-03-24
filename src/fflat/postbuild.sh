bc7f688443fb07d790f119fc5b9bf27a0147db9e
shubhamkumar13 <kumar.shubham.off@gmail.com>
2025-03-24 09:09:32 +0000

    update postbuild.sh for wsl reasons and fflat.fsproj for release

--- src/fflat/postbuild.sh
+++ src/fflat/postbuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-__SOURCE_DIRECTORY__=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+__SOURCE_DIRECTORY__=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 OutputPath=$1
 Configuration=$2
@@ -17,17 +17,17 @@ echo "Bflat framework: $BflatFramework"
 #exit 0
 
 # copy the fflat assemblies to the output directory
-cp -r "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/ref" "$OutputPath"
-cp -r "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/lib" "$OutputPath"
-cp -r "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/lib64" "$OutputPath"
-cp -r "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/bin" "$OutputPath"
+cp -r "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/ref" "$OutputPath"
+cp -r "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/lib" "$OutputPath"
+cp -r "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/lib64" "$OutputPath"
+cp -r "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/bin" "$OutputPath"
 
-# # 
-cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/libobjwriter.so" "$OutputPath"
-cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/libjitinterface_x64.so" "$OutputPath"
-cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/libclrjit_win_x64_x64.so" "$OutputPath"
-cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/libclrjit_unix_x64_x64.so" "$OutputPath"
-cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/libclrjit_universal_arm64_x64.so" "$OutputPath"
-cp -v "$__SOURCE_DIRECTORY__/../../bflat/layouts/windows-x64/WindowsAPIs.txt" "$OutputPath"
+# #
+cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/libobjwriter.so" "$OutputPath"
+cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/libjitinterface_x64.so" "$OutputPath"
+cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/libclrjit_win_x64_x64.so" "$OutputPath"
+cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/libclrjit_unix_x64_x64.so" "$OutputPath"
+cp "$__SOURCE_DIRECTORY__/../../bflat/src/bflat/bin/$Configuration/$BflatFramework/linux-x64/libclrjit_universal_arm64_x64.so" "$OutputPath"
+# cp -v "$__SOURCE_DIRECTORY__/../../bflat/layouts/linux-glibc-x64/WindowsAPIs.txt" "$OutputPath"
 # cp -v "$__SOURCE_DIRECTORY__/../../bflat/layouts/linux-glibc-x64/lib/linux/zerolib.dll" "$OutputPath/lib/"
-# cp -v "$__SOURCE_DIRECTORY__/../../bflat/layouts/linux-glibc-x64/lib/linux/x64/zerolib.dll" "$OutputPath/lib/"
+cp -v "$__SOURCE_DIRECTORY__/../../bflat/layouts/linux-glibc-x64/lib/linux/x64/zerolib.dll" "$OutputPath/lib/"
